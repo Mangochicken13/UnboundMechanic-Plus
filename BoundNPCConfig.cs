73620c281a3f437107f02c394e5602efa8117681
Mangochicken <mangochkn@gmail.com>
2026-01-01 15:04:54 +0800

    name cleanup + start config update

---
+++ BoundNPCConfig.cs
@@ -0,0 +1,250 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Config;
+
+namespace UnboundMechanic
+{
+    public class BoundNPCConfig : ModConfig { // Vanilla bound NPCs
+        public override ConfigScope Mode => ConfigScope.ServerSide;
+
+        // The "AlwaysAvailable" configs will let that respective NPC spawn in immediately,
+        // regardless of other conditions that normally have to be met
+        // These should all default to false, and tell the player about sequence breaks with a tooltip
+
+        // TODO: Check if there's a better/cleaner way of writing the presets
+        #region Presets
+        [Header("Presets")]
+        public bool None {
+            get { 
+                return (
+                    !EnableStylist &&
+                    !EnableAngler &&
+                    !EnableGolfer &&
+                    !EnableGoblin &&
+                    !GoblinAlwaysAvailable &&
+                    !EnableMechanic &&
+                    !MechanicAlwaysAvailable &&
+                    !EnableTavernkeep &&
+                    !TavernkeepAlwaysAvailable &&
+                    !EnableWizard &&
+                    !WizardAlwaysAvailable &&
+                    !EnableTaxCollector &&
+                    !TaxCollectorAlwaysAvailable
+                );
+            }
+            set {
+                if (value) {
+                    // Disable everything
+                    EnableStylist = false;
+                    EnableAngler = false;
+                    EnableGolfer = false;
+                    EnableGoblin = false;
+                    GoblinAlwaysAvailable = false;
+                    EnableMechanic = false;
+                    MechanicAlwaysAvailable = false;
+                    EnableTavernkeep = false;
+                    TavernkeepAlwaysAvailable = false;
+                    EnableWizard = false;
+                    WizardAlwaysAvailable = false;
+                    EnableTaxCollector = false;
+                    TaxCollectorAlwaysAvailable = false;
+                }
+            }
+        }
+
+        public bool OnlyUntie
+        {
+            get => EnableStylist && EnableAngler && EnableGolfer && EnableGoblin && EnableMechanic && EnableTavernkeep && EnableWizard && EnableTaxCollector
+                && !GoblinAlwaysAvailable && !TavernkeepAlwaysAvailable && !MechanicAlwaysAvailable && !WizardAlwaysAvailable && !TaxCollectorAlwaysAvailable;
+            set
+            {
+                if (value)
+                {
+                    // Enable unbinding bound NPCs
+                    EnableStylist = true;
+                    EnableAngler = true;
+                    EnableGolfer = true;
+                    EnableGoblin = true;
+                    EnableMechanic = true;
+                    EnableTavernkeep = true;
+                    EnableWizard = true;
+                    EnableTaxCollector = true;
+
+                    // Set the instant availabilities to false
+                    GoblinAlwaysAvailable = false;
+                    TavernkeepAlwaysAvailable = false;
+                    MechanicAlwaysAvailable = false;
+                    WizardAlwaysAvailable = false;
+                    TaxCollectorAlwaysAvailable = false;
+                }
+            }
+        }
+
+        public bool BoundAlwaysAvailable
+        {
+            get => EnableStylist && EnableAngler && EnableGolfer && EnableGoblin && EnableMechanic && EnableTavernkeep && EnableWizard && EnableTaxCollector
+                && GoblinAlwaysAvailable && TavernkeepAlwaysAvailable && MechanicAlwaysAvailable && WizardAlwaysAvailable && TaxCollectorAlwaysAvailable;
+            set
+            {
+                if (value)
+                {
+                    // Enable unbinding bound NPCs
+                    EnableStylist = true;
+                    EnableAngler = true;
+                    EnableGolfer = true;
+                    EnableGoblin = true;
+                    EnableMechanic = true;
+                    EnableTavernkeep = true;
+                    EnableWizard = true;
+                    EnableTaxCollector = true;
+
+                    // Set the instant availabilities to true
+                    GoblinAlwaysAvailable = true;
+                    TavernkeepAlwaysAvailable = true;
+                    MechanicAlwaysAvailable = true;
+                    WizardAlwaysAvailable = true;
+                    TaxCollectorAlwaysAvailable = true;
+                }
+            }
+        }
+
+        #endregion
+
+        #region Bound NPCs
+
+        [Header("Stylist")]
+        public bool EnableStylist { get; set; }
+
+        [Header("Angler")]
+        public bool EnableAngler { get; set; }
+
+        [Header("Golfer")]
+        public bool EnableGolfer { get; set; }
+
+        [Header("Goblin")]
+        [DefaultValue(true)]
+        public bool EnableGoblin { get; set; }
+        public bool GoblinAlwaysAvailable { get; set; }
+
+        [Header("Tavernkeep")]
+        public bool EnableTavernkeep { get; set; }
+        public bool TavernkeepAlwaysAvailable { get; set; }
+
+        [Header("Mechanic")]
+        [DefaultValue(true)]
+        public bool EnableMechanic { get; set; }
+        public bool MechanicAlwaysAvailable { get; set; }
+
+        [Header("Wizard")]
+        public bool EnableWizard { get; set; }
+        public bool WizardAlwaysAvailable { get; set; }
+
+        [Header("TaxCollector")]
+        public bool EnableTaxCollector { get; set; }
+        public bool TaxCollectorAlwaysAvailable { get; set; }
+
+        #endregion
+    }
+
+    public class EveryOtherNPCConfig : ModConfig
+    {
+        // TODO: Revamp this class to be for restrictionless NPC spawning
+        public override ConfigScope Mode => ConfigScope.ServerSide;
+
+        // Attempt to update the list when the config changes
+        public override void OnChanged() {
+            if (!Main.gameMenu)
+                ModContent.GetInstance<TownNPCSpawnSystem>().UpdateSpawnableTownNPCs();
+            
+            base.OnChanged();
+        }
+
+        private bool _everyoneCanSpawnFromStart;
+        private bool _allowRestrictionlessSpawning;
+
+        [Header("Cheats")]
+        public bool EveryoneCanSpawnFromStart {
+            get { return _everyoneCanSpawnFromStart && _allowRestrictionlessSpawning; }
+            set {
+                if (value) {
+                    _everyoneCanSpawnFromStart = true;
+                    _allowRestrictionlessSpawning = true;
+                }
+                else {
+                    _everyoneCanSpawnFromStart = false;
+                }
+            }
+        }
+
+        public bool AllowRestrictionlessSpawning
+        {
+            get { return _allowRestrictionlessSpawning; }
+            set {
+                if (!value) {
+                    _everyoneCanSpawnFromStart = false;
+                    _allowRestrictionlessSpawning = false;
+                }
+                else {
+                    _allowRestrictionlessSpawning = true;
+                }
+            }
+        }
+
+        #region Specific NPCs
+        
+        [Header("NPCList")]
+        [JsonIgnore]
+        [ShowDespiteJsonIgnore]
+        public bool SampleText;
+
+        public List<NPCDefinition> NPCList = new() {
+            // PreHardmode
+            new NPCDefinition(NPCID.Guide),
+            new NPCDefinition(NPCID.Merchant),
+            new NPCDefinition(NPCID.Nurse),
+            new NPCDefinition(NPCID.Demolitionist),
+            new NPCDefinition(NPCID.DyeTrader),
+            new NPCDefinition(NPCID.Angler),
+            new NPCDefinition(NPCID.BestiaryGirl), // Zoologist
+            new NPCDefinition(NPCID.Dryad),
+            new NPCDefinition(NPCID.Painter),
+            new NPCDefinition(NPCID.Golfer),
+            new NPCDefinition(NPCID.ArmsDealer),
+            new NPCDefinition(NPCID.DD2Bartender), // Tavernkeep
+            new NPCDefinition(NPCID.Stylist),
+            new NPCDefinition(NPCID.GoblinTinkerer),
+            new NPCDefinition(NPCID.WitchDoctor),
+            new NPCDefinition(NPCID.Clothier),
+            new NPCDefinition(NPCID.Mechanic),
+            new NPCDefinition(NPCID.PartyGirl),
+
+            // Hardmode
+            new NPCDefinition(NPCID.Wizard),
+            new NPCDefinition(NPCID.TaxCollector),
+            new NPCDefinition(NPCID.Truffle),
+            new NPCDefinition(NPCID.Pirate),
+            new NPCDefinition(NPCID.Steampunker),
+            new NPCDefinition(NPCID.Cyborg),
+            new NPCDefinition(NPCID.SantaClaus),
+            new NPCDefinition(NPCID.Princess),
+
+            // Town Slimes + Animals
+            new NPCDefinition(NPCID.TownSlimeBlue),
+            new NPCDefinition(NPCID.TownSlimeGreen),
+            new NPCDefinition(NPCID.TownSlimeOld),
+            new NPCDefinition(NPCID.TownSlimePurple),
+            new NPCDefinition(NPCID.TownSlimeRainbow),
+            new NPCDefinition(NPCID.TownSlimeRed),
+            new NPCDefinition(NPCID.TownSlimeYellow),
+            new NPCDefinition(NPCID.TownSlimeCopper),
+            new NPCDefinition(NPCID.TownBunny),
+            new NPCDefinition(NPCID.TownCat),
+            new NPCDefinition(NPCID.TownDog),
+        };
+        #endregion
+    }
+}
---
+++ TownNPCSpawnSystem.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using MonoMod.Cil;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Config;
+
+namespace UnboundMechanic
+{
+    public class TownNPCSpawnSystem : ModSystem
+    {
+        public override void Load() {
+            IL_NPC.AI_007_TownEntities += HookPermanentSanta;
+            On_WorldGen.CheckSpecialTownNPCSpawningConditions += On_CheckSpecialTownNPCSpawningConditions;
+            base.Load();
+        }
+
+        private static void HookPermanentSanta(ILContext il) {
+            try {
+                ILCursor c = new ILCursor(il);
+                
+                c.GotoNext(i => i.MatchLdcI4(NPCID.SantaClaus));
+
+                c.Index++;
+
+                // If santa spawning is enabled, return a value of true (1) to stop him being killed
+                c.EmitDelegate<Func<int, int>>((returnValue) => {
+                    if (ModContent.GetInstance<EveryOtherNPCConfig>().AllowRestrictionlessSpawning &&
+                        SpawnableTownNPCS.Exists(npc => npc.type == NPCID.SantaClaus))
+                        return 1;
+
+                    return returnValue;
+                });
+            }
+            catch (Exception e) {
+                MonoModHooks.DumpIL(ModContent.GetInstance<UnboundMechanic>(), il);
+                throw new ILPatchFailureException(ModContent.GetInstance<UnboundMechanic>(), il, e);
+            }
+        }
+
+        private static bool On_CheckSpecialTownNPCSpawningConditions(
+            On_WorldGen.orig_CheckSpecialTownNPCSpawningConditions orig, int type) {
+            if (type == NPCID.Truffle && SpawnableTownNPCS.Exists(npc => npc.type == NPCID.Truffle)) return true;
+
+            bool canSpawn = orig(type);
+            return canSpawn;
+        }
+
+        public int timer;
+
+        // TODO: Update this list on config change, not just world load
+        private static List<NPC> AllTownNPCS = new();
+        public static List<NPC> SpawnableTownNPCS = new();
+
+        public override void OnWorldLoad() {
+            var otherNPCConfig = ModContent.GetInstance<EveryOtherNPCConfig>();
+            
+            // Add all town NPCs to a reference list, skip Travelling Merchant
+            // TODO: Check that skeleton merchant dosen't do the same multiple spawning thing on secret seeds
+            if (otherNPCConfig.AllowRestrictionlessSpawning) {
+                foreach (NPC npc in ContentSamples.NpcsByNetId.Values) {
+                    if (npc.type == NPCID.TravellingMerchant)
+                        continue;
+                    if (npc.townNPC && NPC.TypeToDefaultHeadIndex(npc.type) >= 0)
+                        AllTownNPCS.Add(npc);
+                }
+            }
+
+            if (!otherNPCConfig.EveryoneCanSpawnFromStart && otherNPCConfig.AllowRestrictionlessSpawning)
+            {
+                UpdateSpawnableTownNPCs();
+            }
+        }
+
+        // Prune unloaded/disabled NPCs from the reference list
+        public void UpdateSpawnableTownNPCs() {
+            List<int> types = new();
+            List<NPC> townNPCsTemp = new();
+
+            foreach (NPCDefinition npcDef in ModContent.GetInstance<EveryOtherNPCConfig>().NPCList) {
+                if (!npcDef.IsUnloaded)
+                    types.Add(npcDef.Type);
+            }
+
+            foreach (NPC npc in AllTownNPCS) {
+                if (types.Contains(npc.type))
+                    townNPCsTemp.Add(npc);
+            }
+            SpawnableTownNPCS = townNPCsTemp;
+        }
+
+        public override void PostUpdateNPCs()
+        {
+            // Hopefully avoid breaking census for multiplayer clients?
+            // Maybe check for the mod on the server?
+            if (Main.netMode == NetmodeID.MultiplayerClient)
+                return;
+            
+            if (ModContent.GetInstance<EveryOtherNPCConfig>().AllowRestrictionlessSpawning) {
+                foreach (NPC npc in SpawnableTownNPCS) {
+                    Main.townNPCCanSpawn[npc.type] = true;
+                }
+            }
+
+            timer++;
+            if (timer >= 120) {
+                timer = 0;
+
+                var config = ModContent.GetInstance<BoundNPCConfig>();
+                if (config == null)
+                    return;
+
+                // NPC.saved[name] for trapped NPCs
+                // NPC.unlocked[name]spawn for non "trapped" NPCs
+
+                if (config.EnableAngler)
+                    AnglerCheck();
+
+                if (config.EnableStylist)
+                    StylistCheck();
+
+                if (config.EnableGolfer)
+                    GolferCheck();
+
+                if (config.EnableGoblin)
+                    GoblinCheck();
+
+                if (config.EnableTavernkeep)
+                    TavernkeepCheck();
+
+                if (config.EnableMechanic)
+                    MechanicCheck();
+
+                if (config.EnableWizard)
+                    WizardCheck();
+
+                if (config.EnableTaxCollector)
+                    TaxCollectorCheck();
+            }
+        }
+
+        #region Specific NPC Checks
+        public static void AnglerCheck()
+        {
+            if (!NPC.savedAngler)
+                NPC.savedAngler = true;
+        }
+
+        public static void StylistCheck()
+        {
+            if (!NPC.savedStylist)
+                NPC.savedStylist = true;
+        }
+
+        public static void GolferCheck()
+        {
+            if (!NPC.savedGolfer)
+                NPC.savedGolfer = true;
+        }
+
+        public static void GoblinCheck()
+        {
+            if (!NPC.savedGoblin && (NPC.downedGoblins || ModContent.GetInstance<BoundNPCConfig>().GoblinAlwaysAvailable))
+                NPC.savedGoblin = true;
+        }
+
+        public static void TavernkeepCheck()
+        {
+            if (!NPC.savedBartender && (NPC.downedBoss2 || ModContent.GetInstance<BoundNPCConfig>().TavernkeepAlwaysAvailable))
+                NPC.savedBartender = true;
+        }
+
+        public static void MechanicCheck()
+        {
+            if (!NPC.savedMech && (NPC.downedBoss3 || ModContent.GetInstance<BoundNPCConfig>().MechanicAlwaysAvailable))
+                NPC.savedMech = true;
+        }
+
+        public static void WizardCheck()
+        {
+            if (!NPC.savedWizard && (Main.hardMode || ModContent.GetInstance<BoundNPCConfig>().WizardAlwaysAvailable))
+                NPC.savedWizard = true;
+
+        }
+
+        public static void TaxCollectorCheck()
+        {
+            if (!NPC.savedTaxCollector && (Main.hardMode || ModContent.GetInstance<BoundNPCConfig>().TaxCollectorAlwaysAvailable))
+                NPC.savedTaxCollector = true;
+        }
+        #endregion
+    }
+}
